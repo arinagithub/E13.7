beaefef50e6140b73d9559e6216737981c1a80e7
arinagithub <81713764+arinagithub@users.noreply.github.com>
2021-12-16 03:07:06 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const heading = document.createElement('h1');
+heading.textContent = 'Как интересно!';
+
+// добавляем заголовок в DOM
+const root = document.querySelector('#root');
+root.append(heading);
+
+console.log(1);
